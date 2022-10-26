dedf0bcdea6abdd1dddcdb510dc18caf6e49c6a1
vvargic <vjekoslav.vargic@inetec.hr>
2022-10-26 09:30:53 +0200

    Falcon D config

--- riverdi_modules/modules.h
+++ riverdi_modules/modules.h
@@ -10,13 +10,13 @@
  * Embedded Video Engine Series
  */
 
-#define EVE_4
+//#define EVE_4
 //#define EVE_4_INTERNAL_OSC
 #define EVE_4_EXTERNAL_OSC
 //#define IPS_35
 //#define IPS_43
 //#define IPS_50
-#define IPS_70
+//#define IPS_70
 //#define IPS_101
 //#define NTP_70
 
